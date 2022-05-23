d976e9771ae82b755b043de92d1b5ba93132941b
CarlosWTMachado <carloswtmachado@gmail.com>
2022-05-23 18:08:59 -0300

    inicio pagina sucesso

--- src/components/Pages/Success.js
+++ src/components/Pages/Success.js
@@ -1,3 +1,50 @@
-export default function Success () {
-	return;
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Success ({title, date, name}) {
+    return (
+        <>
+            <PageTitle>
+                <p>Pedido feito com sucesso!</p>
+            </PageTitle>
+            <InfoBlock>
+                <p>Filme e sessão</p>
+                <p>{title}</p>
+                <p>{date} {name}</p>
+            </InfoBlock>
+            <InfoBlock>
+                <p>Ingressos</p>
+                <p>Assento(s) {}</p>
+            </InfoBlock>
+            <InfoBlock>
+                <p>Comprador</p>
+                <p>Nome: {}</p>
+                <p>CPF: {}</p>
+            </InfoBlock>
+        </>
+
+    );
 }
+
+const PageTitle = styled.div`
+	margin-top: 100px;
+	margin-bottom: 30px;
+	margin-left: 45px;
+	p {
+		font-family: 'Roboto',sans-serif;
+		font-size: 24px;
+		font-weight: bolder;
+		color: #247A6B;
+	}
+`;
+
+const InfoBlock = styled.div`
+	margin-left: 30px;
+	margin-bottom: 100px;
+	p {
+		font-weight: bolder;
+		font-family: 'Roboto', sans-serif;
+		font-size: 24px;
+		color: black;
+	}
+`;
