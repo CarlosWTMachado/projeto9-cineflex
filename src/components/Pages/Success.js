062ef5ab2b3e9d4546b0067ce0f7d256d371b89a
CarlosWTMachado <carloswtmachado@gmail.com>
2022-05-23 18:40:23 -0300

    botao retorna a home

--- src/components/Pages/Success.js
+++ src/components/Pages/Success.js
@@ -1,33 +1,45 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useLocation } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
 export default function Success (props) {
 	const location = useLocation();
 	const {movieTitle, movieDate, movieWeekday, assentos, userName, userCpf} = location.state;
+	let navigate = useNavigate();
     return (
         <>
             <PageTitle>
                 <p>Pedido feito com sucesso!</p>
             </PageTitle>
             <InfoBlock>
-                <p>Filme e sessão</p>
+                <p className='bolder'>Filme e sessão</p>
                 <p>{movieTitle}</p>
                 <p>{movieDate} {movieWeekday}</p>
             </InfoBlock>
             <InfoBlock>
-                <p>Ingressos</p>
-                <p>Assento(s) {assentos}</p>
+                <p className='bolder'>Ingressos</p>
+				{ assentos.map(seat => (<p>Assento {seat}</p>))}
             </InfoBlock>
             <InfoBlock>
-                <p>Comprador</p>
+                <p className='bolder'>Comprador</p>
                 <p>Nome: {userName}</p>
                 <p>CPF: {userCpf}</p>
             </InfoBlock>
+            <Home type='button' onClick={() => navigate("/")}>Voltar para Home</Home>
         </>
 
     );
 }
+
+const Home = styled.button`
+	margin-left: 75px;
+	width: 225px;
+	height: 42px;
+
+	background: #E8833A;
+	border-radius: 3px;
+`;
 
 const PageTitle = styled.div`
 	margin-top: 100px;
@@ -44,8 +56,10 @@ const PageTitle = styled.div`
 const InfoBlock = styled.div`
 	margin-left: 30px;
 	margin-bottom: 100px;
-	p {
+	.bolder{
 		font-weight: bolder;
+	}
+	p {
 		font-family: 'Roboto', sans-serif;
 		font-size: 24px;
 		color: black;
